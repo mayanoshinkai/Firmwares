d62c54c10264f2026b49690e7d9ddb4c1cceebfe
shinkaimayano <98560269+shinkaimayano@users.noreply.github.com>
2023-08-14 02:19:48 -0700

    added electrolytes rev2.0 vial

---
+++ electrolytes/rev2.0/Community Pinout (RP2040)/Source/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define FORCE_NKRO
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+
+/* #define WS2812_DI_PIN GP26 */
+/* #define RGB_MATRIX_LED_COUNT ???*/
+
+/* OLED_ENABLE = yes*/
+/* OLED_DRIVER = SSD1306*/
+/* OLED_TRANSPORT = i2c*/
